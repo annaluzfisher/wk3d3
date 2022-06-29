a860ec17480432592d6e23fbb5b17d1ee5e82d1c
annaluzfisher <annaluzfisher@gail.com>
2022-06-29 11:44:59 -0700

    second test

--- recursion2.rb
+++ recursion2.rb
@@ -1,1 +1,22 @@
 # something
+
+def range_iterative(start, last)
+    count = start
+    new_arr = [start]
+    while counter < last
+        new_arr << new_arr[-1] + 1
+        count += 1
+    end
+    new_arr
+end
+
+def range_recursive(start, last)
+    
+
+end
+
+#count 
+#while count is less than end
+#new array [start]
+#shovel into array << arr[-1] + 1
+#increase count
