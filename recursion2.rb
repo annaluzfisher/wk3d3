9a7fd56def9da0f505d22d1d599e886ab9e42945
annaluzfisher <annaluzfisher@gail.com>
2022-06-29 17:31:49 -0700

    last commit?

--- recursion2.rb
+++ recursion2.rb
@@ -87,13 +87,74 @@ def bi_search(arr, num)
             l+1 + var
         end
      end
+end
+
+# p bi_search([1, 2, 3], 1) # => 0
+# p bi_search([2, 3, 4, 5], 3) # => 1
+# p bi_search([2, 4, 6, 8, 10], 6) # => 2
+# p bi_search([1, 3, 4, 5, 9], 5) # => 3
+# p bi_search([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bi_search([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bi_search([1, 2, 3, 4, 5, 7], 6) # => nil
+
+def merge_sort(arr)
+    l = arr.length/2
+    return arr if arr.length == 1
+    left = arr[0...l]
+    right = arr[l..-1]
+
+    merge(merge_sort(left), merge_sort(right))
 
 end
 
-p bi_search([1, 2, 3], 1) # => 0
-p bi_search([2, 3, 4, 5], 3) # => 1
-p bi_search([2, 4, 6, 8, 10], 6) # => 2
-p bi_search([1, 3, 4, 5, 9], 5) # => 3
-p bi_search([1, 2, 3, 4, 5, 6], 6) # => 5
-p bi_search([1, 2, 3, 4, 5, 6], 0) # => nil
-p bi_search([1, 2, 3, 4, 5, 7], 6) # => nil
+def merge(arr1,arr2=[])
+#   if arr1.length == 1 && arr2.length == 1
+#    merged = arr1 + arr2
+#   return [ merged.min, merged.max ]
+#   end
+    
+    return [] if arr1.nil? 
+    return [] if arr2.nil? 
+    if arr1==[] && arr2 != []
+        return arr2 
+    elsif arr1 != [] && arr2 == []
+        return arr1
+    end
+    if arr1[0] >= arr2[0] 
+        [arr2[0]] + merge(arr1, arr2[1..-1])
+    
+    else arr2[0] >= arr1[0]
+        [arr1[0]] + merge(arr1[1..-1], arr2)
+   
+    end
+end
+
+#merge sort splits 
+#base case sends it merge_sort
+
+# arr = [3,27,38,43]
+# b = [9,10,82]
+# c = [22,23,55,66,77]
+
+# # print merge(arr,c)
+
+#  print merge_sort ([3,27,38,43,22,3,9,10,82])
+
+def subsets(arr)
+    return [] if arr == [] || arr == nil
+     return arr if arr.length == 1 
+   
+    chunks = subsets(arr[0..-2])
+    p chunks
+    chunks + chunks.map do |subs|
+        [subs] + [arr[-1]]
+    end
+end
+
+p subsets([]) # => [[]]
+p subsets([1]) # => [[], [1]]
+p subsets([1, 2]) # => [[], [1], [2], [1, 2]]
+p subsets([1, 2, 3])
+# => [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+    
